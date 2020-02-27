d547ea134334021a91f24bd80b16398599cb3d72
smsazad7 <smsazad7@gmail.com>
2020-02-27 23:49:45 +0300

    new

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import mainBcg1 from "../images/mainBcg1.jpeg";
+import mainBcg2 from "../images/mainBcg2.jpeg";
 
 export default function Hero({ img, title, max, children }) {
   return (
@@ -16,21 +16,20 @@ export default function Hero({ img, title, max, children }) {
 const HeroWrapper = styled.div`
   text-align: center;
   display: flex;
-
+  align-items: center;
   justify-content: center;
   min-height: ${props => (props.newmax ? "100vh" : "60vh")};
   background: linear-gradient(var(--primaryRGBA), var(--primaryRGBA)),
     url(${props => props.img}) center/cover no-repeat;
-  color: var(--primaryColor);
+  color: var(--mainWhite);
   text-transform: uppercase;
   .title {
     padding-top: 2rem;
     font-size: 3.5rem;
     text-shadow: 4px 4px 2px rgba(0, 0, 0, 0.5);
-    margin-top:9rem
   }
 `;
 
 Hero.defaultProps = {
-  img: mainBcg1
+  img: mainBcg2
 };
