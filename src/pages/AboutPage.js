9e3b32b1d89b057f2d52063b1391b091bf897604
smsazad7 <smsazad7@gmail.com>
2020-02-29 00:48:52 +0300

    azadall

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,14 +1,12 @@
 import React from "react";
 import Info from "../components/AboutPage/Info";
 import Hero from "../components/Hero";
-import aboutBcg from "../images/aboutBcg.jpeg";
+import aboutBcg2 from "../images/aboutBcg2.jpeg";
 
 export default function AboutPage() {
   return (
     <>
-      <Hero img={aboutBcg}  
-        
-      />
+      <Hero img={aboutBcg2} />
       <Info />
     </>
   );
